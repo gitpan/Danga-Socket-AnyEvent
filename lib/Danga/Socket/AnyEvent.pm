62d79e50a94fded3c2779478d25c2807c071f970
Sam Vilain <sam@vilain.net>
2012-06-14 18:39:43 +0000

    Import of SAMV/Danga-Socket-AnyEvent-0.03 from CPAN.
    gitpan-cpan-distribution: Danga-Socket-AnyEvent
    gitpan-cpan-version:      0.03
    gitpan-cpan-path:         SAMV/Danga-Socket-AnyEvent-0.03.tar.gz
    gitpan-cpan-author:       SAMV
    gitpan-cpan-maturity:     released

--- lib/Danga/Socket/AnyEvent.pm
+++ lib/Danga/Socket/AnyEvent.pm
@@ -60,7 +60,7 @@ Epoll/KQueue/etc rather than other, less efficient mechanisms.
 package Danga::Socket::AnyEvent;
 
 use vars qw{$VERSION};
-$VERSION = "0.02";
+$VERSION = "0.03";
 use Carp;
 
 BEGIN {
@@ -962,9 +962,15 @@ sub watch_read {
                 $self->event_read() unless $self->{closed};
             }),
         );
+        if ( $PushBackSet{$fd} ) {
+            $self->{event_watch} |= POLLIN;
+        }
     }
     else {
         $watchers->[0] = undef;
+        if ( $PushBackSet{$fd} ) {
+            $self->{event_watch} &= ~POLLIN;
+        }
     }
 }
 
